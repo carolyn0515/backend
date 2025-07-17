d97b7c0f7e9ef6139a1da9514ffb39496841cbc5
carolyn <neige040515@gmail.com>
2025-07-17 22:45:28 +0900

    7/17

--- src/main/resources/static/js/article.js
+++ src/main/resources/static/js/article.js
@@ -19,7 +19,7 @@ if (deleteButton) {
 
 // 수정
 if (modifyButton) {
-    modifyButton.addEventListener('click', () => {
+    modifyButton.addEventListener('click', event => {
         let params = new URLSearchParams(location.search);
         let id = params.get('id');
 
@@ -57,5 +57,24 @@ if (createButton) {
             alert('등록이 완료되었습니다.');
             location.replace('/articles');
         });
+    });
+}
+
+const createButton = document.getElementById("create-btn");
+if (createButton) {
+    createButton.addEvenListener("click", (event) => {
+        fetch("/api/articles, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+            title: document.getElementById("title").value,
+            content: document.getElementById("content").value,
+            }),
+            }).then(() => {
+            alert("등록 완료되었습니다.");
+            location.replace("/articles");
+            });
     });
 }
